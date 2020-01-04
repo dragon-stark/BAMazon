349567d984d721242d9891b28f347c0383a6bf91
DStark <hindash@email.arizona.edu>
2020-01-03 19:54:47 -0700

    db connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var dotenv = require("dotenv")
 var mysql = require("mysql")
 var inquirer = require("inquirer")
+var chalk = require("chalk")
 
 dotenv.config();
 
@@ -10,4 +11,102 @@ var connection = mysql.createConnection({
   user: process.env.user,
   password: process.env.db_pass,
   database: "bamazon_db"
-})
+});
+
+connection.connect(function (error)
+{
+  if (error) throw error;
+  start();
+});
+
+//function to validate the user's input which returns true if the value is valid or error otherwise
+function validateInput (value)
+{
+  var integer = Number.isInteger(parseFloat(value));
+  var sign = Math.sign(value);
+
+  if (integer && (sign === 1)) {
+    return true
+  } else {
+    return "Please enter a whole non-zero number"
+  }
+}
+
+// function to ask user questions
+function start ()
+{
+  inquirer.prompt([{
+    type: "input",
+    name: "item_id",
+    message: "Please enter your item ID",
+    validate: validateInput,
+    filter: Number
+  },
+  {
+    type: "input",
+    name: "stock_quantity",
+    message: "Please enter a quantity for your item",
+    validate: validateInput,
+    filter: Number
+  }
+  ]).then(function (input)
+  {
+    // this pulls from the database
+    var item = input.item_id;
+    var quantity = input.stock_quantity;
+
+    var queryStr = "SELECT * FROM products WHERE ? ";
+
+    connection.query(queryStr, { item_id: item }, function (err, data)
+    {
+      if (err) throw err;
+      if (data.length === 0) {
+        console.log(chalk.bgMagenta("Error invalid, please enter a valid ID."));
+        displayInventory();
+      } else {
+        var productData = data[0];
+        if (quantity <= productData.quantity) {
+          console.log(chalk.yellowBright("Great, you have succesfully placed your order."));
+          var updateQueryStr = "UPDATE products SET stock_quantity = " + (productData.quantity - quantity) + "WHERE item_id = " + item;
+
+          connection.query(updateQueryStr, function (err, data)
+          {
+            if (err) throw err;
+            console.log(chalk.yellowBright("Your order has been placed. Your total is $" + productData.price * quantity));
+            console.log(chalk.blueBright("Thank you for shopping Bamazon."))
+
+            connection.end();
+          })
+        } else {
+          console.log(chalk.yellowBright("Sorry, insufficient quantity"));
+          console.log(chalk.redBright("Please, modify your order."));
+
+          displayInventory();
+        }
+      }
+    })
+  })
+}
+
+function displayInventory ()
+{
+  queryStr = "SELECT * FROM products";
+
+  connection.query(queryStr, function (err, data)
+  {
+    if (err) throw err;
+    console.log(chalk.bgYellowBright("Existing inventory: "));
+
+    var allItems = "";
+    for (var i = 0; i < data.length; i++) {
+      allItems = "";
+      allItems += "Item ID: " + data[i].item_id + "//";
+      allItems += "Product name: " + data[i].product_name + "//";
+      allItems += "Department: " + data[i].department_name + "//";
+      allItems += "Price: " + data[i].price + "//";
+
+      console.log(chalk.blueBright.bold(allItems));
+    }
+    start();
+  })
+}
--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ CREATE database bamazon_db;
 USE bamazon_db;
 CREATE TABLE products (
   item_id INT (10) NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR (30) NOT NULL,
-  department_name VARCHAR(30) NOT NULL,
+  product_name VARCHAR (100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
   price DECIMAL (10, 2) NOT NULL,
   stock_quantity INT (30) NOT NULL,
   PRIMARY KEY (item_id)
@@ -16,24 +16,14 @@ INSERT INTO products (
     stock_quantity
   )
 VALUES
-  (
-    "Cards Against Humanity",
-    "Toys & Games",
-    10.00,
-    25
-  ),
+  ("Card Game", "Toys & Games", 10.00, 25),
   ("Teddy Bear", "Toys & Games", 5.00, 10),
   ("Barbie", "Toys & Games", 10.00, 15),
   ("Action Figure", "Toys & Games", 10.00, 20),
   ("Xbox Gift Cards", "Toys & Games", 10.00, 25),
   ("Nintendo Game", "Toys & Games", 50.00, 25),
   ("Monopoly Game", "Toys & Games", 30.00, 10),
-  (
-    "What do you Meme Game",
-    "Toys & Games",
-    10.00,
-    10
-  ),
+  ("Guess Who", "Toys & Games", 10.00, 10),
   ("Kinetic Sand", "Toys & Games", 2.00, 10),
   ("UNO Card Game", "Toys & Games", 4.00, 15),
   ("Poker Card Game", "Toys & Games", 8.00, 15);
